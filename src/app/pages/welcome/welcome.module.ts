806b12b2b3d98ca51273cd3c9ca4a36fe81361d8
u531677 <ANBongioanni@teco.com.ar>
2023-09-05 10:09:45 -0300

    cambios diseño

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomePage } from './welcome.page';
+import { IonicModule } from '@ionic/angular';
+import { WelcomePageRoutingModule } from './welcome-routing.module';
 
 @NgModule({
   declarations: [WelcomePage],
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule, WelcomePageRoutingModule],
 })
 export class WelcomeModule {}
